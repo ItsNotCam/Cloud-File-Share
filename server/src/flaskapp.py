9e99b12fe31e811f1cc54f168b7bd8163bb18b2d
ItsNotCam <camallanyoung@gmail.com>
2024-01-06 21:24:44 -0500

    docker-compose build order enforced

--- server/src/flaskapp.py
+++ server/src/flaskapp.py
@@ -7,32 +7,31 @@ from flask import Flask
 from flask_restful import Api, Resource
 
 load_dotenv()
+TABLES = []
+try:
+  cnx = mysql.connector.connect(
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("MYSQL_DATABASE"),
+    port=os.getenv("DB_PORT")
+  )
+
+  with cnx.cursor() as cursor:
+    res = cursor.execute("SHOW TABLES")
+    for r in cursor.fetchall():
+      print(r[0])
+      TABLES.append(r[0])
+
+    cnx.close()
+except mysql.connector.Error as err:
+  print("Something went wrong: {}".format(err))
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
   def get(self):
-    TABLES = []
-    try:
-      cnx = mysql.connector.connect(
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("MYSQL_DATABASE"),
-        port=os.getenv("DB_PORT")
-      )
-
-      with cnx.cursor() as cursor:
-        res = cursor.execute("SHOW TABLES")
-        for r in cursor.fetchall():
-          print(r[0])
-          TABLES.append(r[0])
-
-        cnx.close()
-    except mysql.connector.Error as err:
-      print("Something went wrong: {}".format(err))
-
     return { 'ok': TABLES }
   
 api.add_resource(HelloWorld, '/ok')
