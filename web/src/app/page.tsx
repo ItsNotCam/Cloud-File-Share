95d32f1b2aa3c4fd32905b836b6a43a28888f1f8
ItsNotCam <camallanyoung@gmail.com>
2024-01-07 12:51:32 -0500

    docker final setup

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,17 +5,7 @@ interface ok {
   tables: string[];
 }
 
-
 export default async function Home() {
-  // const [data, setData] = useState<ok>({"tables": ["waiting"]});
-  
-  // useEffect(() => {
-    //   console.log(`\n\n\nREMOTE HOST\n192.168.1.98\n\n\n`)
-    //   axios.get(`http://192.168.1.98:5000/api/db/tables`)
-    //     .then(res => setData(res.data))
-    //     .catch(err => console.log(err))
-    // }, []);
-
   var data: ok = {tables: ["waiting"]}
   await axios.get(`http://${process.env.API_HOST}:${process.env.API_PORT}/api/db/tables`)
     .then(res => {
