8b13a33e1a55d9c8812abb62250de2e33d57f374
ItsNotCam <camallanyoung@gmail.com>
2024-01-06 22:06:03 -0500

    changed mysql pw encryption type

--- db/init.sh
+++ db/init.sh
@@ -1,6 +1,6 @@
 cd /docker-entrypoint-initdb.d
 
-mysql -u root -pok -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';";
+mysql -u root -pok -e "CREATE USER '$DB_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';";
 mysql -u root -pok -e "GRANT SELECT ON *.* TO '$DB_USER'@'%';"
 mysql -u root -pok -e "CREATE DATABASE OK;"
 mysql -u root -pok < init.sql;
