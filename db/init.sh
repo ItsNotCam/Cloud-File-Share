c860a06ca142b71c85d6f329fb1aadeed2303187
ItsNotCam <camallanyoung@gmail.com>
2024-01-06 23:48:46 -0500

    remove unneeded debug line

--- db/init.sh
+++ db/init.sh
@@ -1,10 +1,4 @@
 cd /docker-entrypoint-initdb.d
 
-echo ""
-echo ""
-echo "CREATE USER '$DB_USER'@'$API_HOST' IDENTIFIED BY '$DB_PASS';";
-echo ""
-echo ""
-
 mysql -u root -pok -e "CREATE USER '$DB_USER'@'$API_HOST' IDENTIFIED BY '$DB_PASS';";
 mysql -u root -pok -e "GRANT SELECT ON *.* TO '$DB_USER'@'$API_HOST';"
