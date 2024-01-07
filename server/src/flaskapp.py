aba5f8d72a5958c36f510906279debdc57e2667b
ItsNotCam <camallanyoung@gmail.com>
2024-01-06 20:11:23 -0500

    setup as server for testing

--- server/src/flaskapp.py
+++ server/src/flaskapp.py
@@ -3,7 +3,11 @@ from pathlib import Path
 import mysql.connector
 import os
 
+from flask import Flask
+from flask_restful import Api, Resource
+
 load_dotenv()
+TABLES = []
 
 cnx = mysql.connector.connect(
   user=os.getenv("DB_USER"),
@@ -17,17 +21,18 @@ with cnx.cursor() as cursor:
   res = cursor.execute("SHOW TABLES")
   for r in cursor.fetchall():
     print(r[0])
+    TABLES.append(r[0])
 
   cnx.close()
 
-# app = Flask(__name__)
-# api = Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-# class HelloWorld(Resource):
-#   def get(self):
-#     return { 'hello': 'world' }
+class HelloWorld(Resource):
+  def get(self):
+    return { 'ok': TABLES }
   
-# api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/ok')
 
-# if __name__ == '__main__':
-#   app.run(debug=True)
+if __name__ == '__main__':
+  app.run(debug=True)
