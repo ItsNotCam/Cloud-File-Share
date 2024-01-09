ef9765ae0883a9b080e1cc5afaea055c34205dba
ItsNotCam <camallanyoung@gmail.com>
2024-01-09 16:47:10 +0000

    updating api

--- server/src/main.py
+++ server/src/main.py
@@ -1,14 +1,4 @@
-from typing import Annotated
-from dotenv import load_dotenv
-import os
-
-from fastapi import FastAPI, Form
-from pydantic import BaseModel
-
-import mysql.connector
-
-load_dotenv()
-
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -17,63 +7,5 @@ async def root():
   return {"message": "yay its all working"}
 
 @app.get("/api/user")
-def root():
-  TABLES = []
-  try:
-    cnx = mysql.connector.connect(
-      user=os.getenv("DB_USER"),
-      password=os.getenv("DB_PASS"),
-      host=os.getenv("DB_HOST"),
-      database=os.getenv("MYSQL_ROOT_DATABASE"),
-      port=os.getenv("DB_PORT")
-    )
-
-    with cnx.cursor() as cursor:
-      res = cursor.execute("SELECT * FROM USER ORDER BY CREATED_AT")
-      for r in cursor.fetchall():
-        ok = []
-        for a in r:
-          ok.append(a)
-
-        TABLES.append(ok)
-
-      cnx.close()
-  except mysql.connector.Error as err:
-    print("Something went wrong: {}".format(err))
-  
-  return {"tables": TABLES}
-  
-@app.post("/api/user")
-async def setUser(email: Annotated[str, Form()], password: Annotated[str, Form()]):
-  print(f"{email} - {password}")
-
-  try:
-    cnx = mysql.connector.connect(
-      user=os.getenv("DB_USER"),
-      password=os.getenv("DB_PASS"),
-      host=os.getenv("DB_HOST"),
-      database=os.getenv("MYSQL_ROOT_DATABASE"),
-      port=os.getenv("DB_PORT")
-    )
-
-    print("connected")
-
-    with cnx.cursor() as cursor:
-      data = {
-        'email': email, 
-        'password': password
-      }
-
-      sql = (
-        "INSERT INTO USER "
-        "(EMAIL, PASSWORD) "
-        f"VALUES (\"{email}\", \"{password}\")"
-      )
-
-      res = cursor.execute(sql)
-      cnx.commit()
-    cnx.close()
-  except mysql.connector.Error as err:
-    print("Something went wrong: {}".format(err))
-
-  return {"email": email, "password": password}
+def test():
+  return {"lol": "ok"}
--- tests/insert_users.py
+++ tests/insert_users.py
@@ -1,22 +1,33 @@
 import requests, sys, os
 from dotenv import load_dotenv
 
+if len(sys.argv) != 2:
+    print("This script requires one positional argument denoting the number of users to generate")
+    print("Usage: python3 insert_users.py <number here>")
+    exit()
+
 load_dotenv()
 api_host = os.getenv("API_HOST")
 api_port = os.getenv("API_PORT")
 
-headers = { 'User-Agent': 'Mozilla/5.0' }
-
 for i in range(0, int(sys.argv[1])):
     resp = requests.get('https://random-data-api.com/api/bank/random_bank?size=3')
     
-    pas = resp.json()[0]['routing_number']
+    passwd = resp.json()[0]['routing_number']
     bank_name = resp.json()[0]['bank_name']
     bic = resp.json()[0]['swift_bic']
     name = f"{bank_name}@{bic}.com"
 
-    payload = { 'email': name.replace(' ', '_'), 'password': pas }
-    print(payload)
+    payload = { 'email': name.replace(' ', '_'), 'password': passwd }
 
     session = requests.session()
-    resp = session.post(f"http://{api_host}:{api_port}/api/user", headers=headers, data=payload)
+    resp = session.post(
+        url=f"http://{api_host}:{api_port}/api/user", 
+        headers={ "User-Agent": "Mozilla/5.0" }, 
+        data=payload
+    )
+
+    if resp.status_code != 200:
+        print("Failed")
+    else:
+        print(payload)
