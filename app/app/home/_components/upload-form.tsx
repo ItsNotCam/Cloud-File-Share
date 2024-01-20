ca0a43bae681344e12f4ddd2726ee19f5fb12e41
ItsNotCam <camallanyoung@gmail.com>
2024-01-19 23:29:07 -0500

    refactored upload form

--- app/app/home/_components/upload-form.tsx
+++ app/app/home/_components/upload-form.tsx
@@ -9,37 +9,64 @@ interface ValidationErrors {
   errors: string[]
 }
 
-export default function UploadForm(props: {SERVER_SOCKET: string}): JSX.Element {
-  const [file, setFile] = useState<File>()
-  const [uploadingProgress, setUploadingProgress] = useState<number>(0)
-  const [isUploading, setIsUploading] = useState<boolean>(false)
+interface IUploadFormProps {
+  file: File | null
+  uploadingProgress: number
+  isUploading: boolean
+  USERNAME: string
+  PASSWORD: string
+}
 
-  const [username, setUsername] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
+export default function UploadForm(props: {SERVER_SOCKET: string}): JSX.Element {
+  const [state, setState] = useState<IUploadFormProps>({
+    file: null,
+    uploadingProgress: 0,
+    isUploading: false,
+    USERNAME: "",
+    PASSWORD: ""
+  })
+
+  const handleFormChange = (event) => {
+    const newValue: string | File = event.target.name == "file" 
+      ? event.target.files?.[0] 
+      : event.target.value
+
+    setState(prev => ({
+      ...prev,
+      [event.target.name]: newValue
+    }))
+  }
+
+  const handleProgressUpdate = (event: AxiosProgressEvent) => {
+    const progress: number = event.loaded / (event.total || 9999999) * 100
+    setState(prev => ({ 
+      ...prev, 
+      uploadingProgress: progress 
+    }))
+  }
 
   const uploadFile = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    if(!file) {
+    if(!state.file) {
       alert("failed to upload - no file was present")
       return
     }
 
     try {
-      const data = new FormData()
-      data.set('file', file)
-      data.set('filesize', file.size.toString())
+      const data: FormData = new FormData()
+      data.set('file', state.file)
+      data.set('filesize', state.file.size.toString())
       
-      setIsUploading(true)
+      setState({ ...state, isUploading: true })
       axios.post(`http://${props.SERVER_SOCKET}/api/files/upload`, data, {
         headers: { 'Content-Type': 'multipart/form-data' },
-        onUploadProgress: (event: AxiosProgressEvent) => {
-          const progress: number = event.loaded / (event.total || 9999999) * 100
-          setUploadingProgress(progress)
-        }
-      }).then((resp) => {
-        console.log(resp.data)
-        setIsUploading(false)
-        setUploadingProgress(0)
+        onUploadProgress: handleProgressUpdate
+      }).then(_ => {
+        setState(prev => ({ 
+          ...prev, 
+          isUploading: false, 
+          uploadingProgress: 0 
+        }))
       })
     } catch (e: any) {
       console.error(e)
@@ -49,19 +76,17 @@ export default function UploadForm(props: {SERVER_SOCKET: string}): JSX.Element 
   const createUser = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const data = {
-      PASSWORD: password,
-      USERNAME: username
+      PASSWORD: state.PASSWORD,
+      USERNAME: state.USERNAME
     }
     await axios.post(`http://${props.SERVER_SOCKET}/api/users/create`, data, {
       headers: { 'Content-Type': 'application/json' },
-    }).then(resp => {
-      setPassword("")
-      setUsername("")
+    }).then(_ => {
+      setState(prev => ({ ...prev, USERNAME: "", PASSWORD: "" }))
     }).catch(err => {
       const axiosErr: AxiosError = err as AxiosError
       const {errors}: ValidationErrors = axiosErr.response?.data as ValidationErrors
       alert(errors.join(" - "))
-      console.log(axiosErr.response?.data)
     })
   }
 
@@ -76,8 +101,8 @@ export default function UploadForm(props: {SERVER_SOCKET: string}): JSX.Element 
             type="text" 
             name="USERNAME" 
             style={{backgroundColor: "#323239", borderColor: "#1D1D21", color: "white"}}
-            onChange={(e) => setUsername(e.target.value)} 
-            value={username}/> 
+            onChange={handleFormChange} 
+            /> 
         </div>
 
         <br />
@@ -90,8 +115,8 @@ export default function UploadForm(props: {SERVER_SOCKET: string}): JSX.Element 
             id="password"
             name="PASSWORD"
             style={{backgroundColor: "#323239", borderColor: "#1D1D21", color: "white"}}
-            onChange={(e) => setPassword(e.target.value)} 
-            value={password}/> 
+            onChange={handleFormChange} 
+            /> 
         </div>
 
         <br />
@@ -101,10 +126,10 @@ export default function UploadForm(props: {SERVER_SOCKET: string}): JSX.Element 
       <br />
       <br />
       <form onSubmit={(event) => uploadFile(event)}>
-        <input type="file" name="file" onChange={(e) => setFile(e.target.files?.[0])}/>
-        <input type="submit" value="Upload" disabled={file == null}/>
+        <input type="file" name="file" onChange={handleFormChange}/>
+        <input type="submit" value="Upload" disabled={state.file == null}/>
       </form>
-      {isUploading && <><br /><ProgressBar now={uploadingProgress} /></>}
+      {state.isUploading && <><br /><ProgressBar now={state.uploadingProgress} /></>}
     </div>
   )
 }
