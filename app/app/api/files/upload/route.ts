1c34c234ef069cb6ec5631beade15b963a8628dc
ItsNotCam <camallanyoung@gmail.com>
2024-02-08 20:06:55 -0500

    moved file writing to streaming....

--- app/app/api/files/upload/route.ts
+++ app/app/api/files/upload/route.ts
@@ -1,7 +1,12 @@
 // UPLOAD FILE
 import { NextRequest, NextResponse } from "next/server";
-import { rm, writeFile } from "fs";
+import fs from 'fs'
 import {v4 as uuidv4} from 'uuid'
+import { cookies } from "next/headers";
+import DBAuth from "@/lib/db/DBAuth";
+import Logger from "@/lib/logger";
+import { getFileInfo } from "@/lib/util";
+import DBFile from "@/lib/db/DBFiles";
 
 export interface IFileInfo {
   FILE_ID: string
@@ -9,11 +14,6 @@ export interface IFileInfo {
   EXTENSION: string
   NAME: string
 }
-import { cookies } from "next/headers";
-import DBAuth from "@/lib/db/DBAuth";
-import Logger from "@/lib/logger";
-import { getFileInfo } from "@/lib/util";
-import DBFile from "@/lib/db/DBFiles";
 
 async function UploadFile(request: NextRequest): Promise<NextResponse> {
 	Logger.LogReq(request)
@@ -37,27 +37,42 @@ async function UploadFile(request: NextRequest): Promise<NextResponse> {
 		)
 	}
 
-  // Save file to PC
-  const PATH = `${process.env.FILES_DIRECTORY}/${uuidv4()}`
-  const buffer = await file.arrayBuffer()
-    .then(bytes => Buffer.from(bytes))
-  writeFile(PATH, buffer, () => {})
-  
-  // Add entry to database
-	const success = DBFile.SaveFile(FILE_ID, NAME, EXTENSION, USER.ID, PATH, file.size)
+	try {
+		// Save file to PC
+		const PATH = `${process.env.FILES_DIRECTORY}/${uuidv4()}`
+		await FSSaveFile(file, PATH)
 
-  if(!success) {
-		rm(PATH, () => { return "deleted" })
-    return new NextResponse(
-      "Error saving file to the database", { status: 500 }
-    )
-  }
+		// Add entry to database
+		const success = DBFile.SaveFile(FILE_ID, NAME, EXTENSION, USER.ID, PATH, file.size)
+
+		if(!success) {
+			fs.rm(PATH, () => { return "deleted" })
+			return new NextResponse(
+				"Error saving file to the database", { status: 500 }
+			)
+		}
+	} catch(err: any) {
+		Logger.LogErr(`Error saving file ${err.message}`)
+	}
 
 	return NextResponse.json({
 		ID: FILE_ID
 	}, { 
 		status: 200 
 	})
+}
+
+const FSSaveFile = async (file: File, PATH: string): Promise<boolean> => {
+	const uploadStream = fs.createWriteStream(PATH)
+	await file.stream().pipeTo(new WritableStream({
+		write(chunk) {
+			uploadStream.write(chunk)
+		}
+	})).finally(() => {
+		uploadStream.close()
+	})
+
+	return true
 }
 
 export {UploadFile as POST}
